5030c8fb9557c2c4235605e015cdcbfb68e2eb48
Scientist Technologies <science_tech@MacBook-Air.local>
2024-02-18 15:55:12 +0000

    simple table display

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 # This is the main streamlit app
 
+
 import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
