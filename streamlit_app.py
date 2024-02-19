a58a4d4cd17e98a04bb59d0c5ca4981f28548f70
Scientist Technologies <science_tech@MacBook-Air.local>
2024-02-19 12:56:56 +0000

    added simple third person

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,16 @@ if "messages" not in st.session_state:
 
 if prompt := st.chat_input("Hi"):
     prompt_user = prompt
-    sentence_type = f" {random.choice([1,2])} {random.choice([5,6,7])}-word sentences"
+    sentence_type = f" {random.choice([1,2])} sentence. Each with {random.choice([5,6,7])} word"
     print(sentence_type)
-    age = random.choice([7,8])
+    age = random.choice([7,8,9])
     print(age)
-    fear_factor = random.choice(["dark", "death", "blood", "fear", "sad"])
+    fear_factor = random.choice(["dark", "death", "blood", "fear", "sad", "lonely"])
     print(fear_factor)
     prompt_modifier = f"\" with a scary answer. Make it {sentence_type}. Make it sound like {age}-year old girl. Use {fear_factor} in your sentence. Use simple words that a child would use"
     print(prompt_modifier)
     # prompt_openai = f"respond to \""+ prompt_user + "\" with a scary answer. make it {sentence_type} make it sound like a 8 year old girl. use death in your response. use words that kids use."
-    prompt_openai = f"Assume there's a scary stuff toy named Chauncey. I want to chat with it through you. Chauncey is lonely. Respond to \""+ prompt_user + prompt_modifier
+    prompt_openai = f"Assume there's an 8 year old girl. She has a cute but scary stuff toy named Chauncey. I want to chat with it. Chauncey is sinister and lonely. Respond as the girl to \"Ask Chauncey "+ prompt_user + prompt_modifier
     print(prompt_openai)
     st.session_state.messages.append({"role": "user", "content": prompt_openai})
     with st.chat_message("user"):
